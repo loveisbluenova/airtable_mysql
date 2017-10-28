cee4e1e7f970b885938e6cc387a0960b5cb45c18
loveisbluenova <michalstroik@yandex.com>
2017-10-28 07:44:20 +0200

    Update projects.blade.php

--- resources/views/frontend/projects.blade.php
+++ resources/views/frontend/projects.blade.php
@@ -166,7 +166,7 @@ div.dataTables_length select{
   </ul>
  </div>
   <div class="top-bar-title">
-    <a href="http://proposals.votedevin.com/" style="color: #ffffff;"><img src="../../resources/images/logo.png" style="padding-right: 10px;"> Capital Budgets</a>
+    <a href="/" style="color: #ffffff;"><img src="../../resources/images/logo.png" style="padding-right: 10px;"> Capital Budgets</a>
     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
     <i class="fa fa-bars"></i>Menu</button>
   </div>
